3413c83c7e257fb5d3143f98713caabcf55e38c7
Rushda Rasheed <rushirasheed42@gmail.com>
2025-04-12 17:37:21 +0500

    Removed default page.js and added redirect to /portfolio#home

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     eslint: {
-        ignoreDuringBuilds: true,
-     },
-};
-
-export default nextConfig;
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/portfolio#home',
+          permanent: false, // use true if you want a permanent 308 redirect
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
